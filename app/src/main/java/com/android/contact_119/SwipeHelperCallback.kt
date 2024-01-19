9749c52dbc22fdc2e42fbbe21336a8359043bf14
LEE HO <dlho1222@naver.com>
2024-01-19 12:02:11 +0900

    [UI]:삭제 다이얼로그 UI변경

--- app/src/main/java/com/android/contact_119/SwipeHelperCallback.kt
+++ app/src/main/java/com/android/contact_119/SwipeHelperCallback.kt
@@ -1,8 +1,13 @@
 package com.android.contact_119
 
+import android.app.ActionBar.LayoutParams
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ItemTouchHelper.LEFT
@@ -36,13 +41,10 @@ class SwipeHelperCallback(
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
         val position = viewHolder.adapterPosition
         val item = adapter.getPosition(position) as ContactItems.Contents
-
         //오른쪽 스와이프 시 전화연결 //왼쪽 스와이프 시 아이템 삭제
         if (direction == ItemTouchHelper.RIGHT) callConnect(item) else removeContact(item)
-
-
-
     }
+
     override fun getSwipeEscapeVelocity(defaultValue: Float): Float {
         return defaultValue * 20
     }
@@ -56,9 +58,34 @@ class SwipeHelperCallback(
             }
         }
     }
+
     private fun removeContact(item: ContactItems.Contents) {
+        val imageView = ImageView(context).apply {
+            scaleType = ImageView.ScaleType.CENTER_CROP
+            val width = LayoutParams.MATCH_PARENT
+            val height = 500
+            layoutParams = LinearLayout.LayoutParams(width, height).apply {
+                topMargin = 100
+            }
+            adjustViewBounds = true
+            item.thumbnailImage?.let { setImageResource(it) }
+
+        }
+        val textView = TextView(context).apply {
+            textSize = 30f
+            gravity = Gravity.CENTER
+            item.phoneNumber.let { text = it }
+        }
+        val layout = LinearLayout(context).apply {
+            orientation = LinearLayout.VERTICAL
+            addView(imageView)
+            addView(textView)
+        }
+
         AlertDialog.Builder(context).apply {
-            setMessage("연락처를 삭제 하시겠습니까?")
+            setView(layout)
+            setTitle("연락처를 삭제 하시겠습니까?")
+
             setPositiveButton("확인") { _, _ ->
                 item.let {
                     ContactItemManager.contactItems.remove(item)
